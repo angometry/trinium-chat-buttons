19f03a667b89b5c17f3e2df4560f70569c9ab26b
Andrea Gomez <thedarktongo@users.noreply.github.com>
2024-07-18 21:33:09 +0200

    Added log levels

--- scripts/logger.js
+++ scripts/logger.js
@@ -1,25 +1,33 @@
-// Logger.js
-
 export class TriniumLogger {
     constructor(moduleName) {
       this.moduleName = moduleName;
+      this.logLevel = game.settings.get('trinium-chat-buttons', 'logLevel');
     }
   
-    info(...args) {
-      console.log(`${this.moduleName} |`, ...args);
-    }
+    log(level, message, ...args) {
+      const levels = ['debug', 'info', 'warn', 'error'];
+      const currentLevel = levels.indexOf(this.logLevel);
+      const messageLevel = levels.indexOf(level);
   
-    warn(...args) {
-      console.warn(`${this.moduleName} |`, ...args);
-    }
-  
-    error(...args) {
-      console.error(`${this.moduleName} |`, ...args);
-    }
-  
-    debug(...args) {
-      if (game.settings.get(this.moduleName, 'debug')) {
-        console.debug(`${this.moduleName} |`, ...args);
+      if (messageLevel >= currentLevel) {
+        console[level](`${this.moduleName} | ${message}`, ...args);
       }
     }
+  
+    info(message, ...args) {
+      this.log('info', message, ...args);
+    }
+  
+    warn(message, ...args) {
+      this.log('warn', message, ...args);
+    }
+  
+    error(message, ...args) {
+      this.log('error', message, ...args);
+    }
+  
+    debug(message, ...args) {
+      this.log('debug', message, ...args);
+    }
   }
+  
